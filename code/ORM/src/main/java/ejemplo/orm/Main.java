fe527cc5f55245984db2df49a6c69a645cd47a51
kralos-7 <c_rojasx@hotmail.com>
2025-05-24 18:22:09 -0600

    Update code

--- code/ORM/src/main/java/ejemplo/orm/Main.java
+++ code/ORM/src/main/java/ejemplo/orm/Main.java
@@ -1,8 +1,7 @@
-package ejemplo.orm;
 
 public class Main {
-    public static void main(String[] args) {
-        UsuarioDAO dao = new UsuarioDAO();
+     public static void main(String[] args) {
+         UsuarioDAO dao = new UsuarioDAO();
         Usuario nuevo = new Usuario("Juan Pérez");
         dao.insertarUsuario(nuevo);
     }
